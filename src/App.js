56f79f6c1ad9eb3c2ccd595f0487efcb37f7d9b4
Amanullah Aman <amancse.aiub@gmail.com>
2019-12-23 19:52:45 +0600

    Component class, constructor and state

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "Aman",
+      next: "Khan"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Using class component</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>Good Morning {this.state.string}</p>
+          <button
+            onClick={() =>
+              this.setState({
+                next: this.state.string,
+                string: this.state.next
+              })
+            }
           >
-            Learn React
-          </a>
+            Change text
+          </button>
         </header>
       </div>
     );
