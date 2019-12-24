7f4fa31b07a5e58b4a97ee4d5f9c1e8398de42c2
Amanullah Aman <amancse.aiub@gmail.com>
2019-12-24 18:25:57 +0600

    Updates

---
+++ precache-manifest.ba22f7cfd35b11b6089fd5a574dfe68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bafae725d39bd974602186deaf933f",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "2592a394ad651b7ad107",
+    "url": "/react-monster/static/css/main.97b1b9ba.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-monster/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2592a394ad651b7ad107",
+    "url": "/react-monster/static/js/main.2f87fc1e.chunk.js"
+  },
+  {
+    "revision": "7a453121113cd2b5f2a4",
+    "url": "/react-monster/static/js/runtime-main.2b46bef7.js"
+  }
+]);
